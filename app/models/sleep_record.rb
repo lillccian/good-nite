eb622ba479d2d13111ef9b13cbffe6729200e773
eason.lin <lillccian@gmail.com>
2023-01-18 20:09:40 +0800

    Add sleep_record model valid

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -12,4 +12,37 @@
 #
 class SleepRecord < ApplicationRecord
   belongs_to :user
+
+  validates_presence_of :start_at, :end_at
+  validate :time_comparison_valid
+  validate :time_overlap_valid
+
+  before_save :update_sleep_time
+
+  scope :overlap, -> (time){ where('? BETWEEN start_at AND end_at', time) }
+
+  private
+
+  def time_comparison_valid
+    return unless start_at && end_at
+
+    if start_at >= end_at
+      errors.add(:start_at, 'start time should less than end time')
+    end
+  end
+
+  def time_overlap_valid
+    return unless user
+
+    if user.sleep_records.where.not(id: id).overlap(start_at).exists?
+      errors.add(:start_at, 'start time overlap other record')
+    end
+    if user.sleep_records.where.not(id: id).overlap(end_at).exists?
+      errors.add(:end_at, 'end time overlap other record')
+    end
+  end
+
+  def update_sleep_time
+    self.sleeping_time = end_at - start_at
+  end
 end
