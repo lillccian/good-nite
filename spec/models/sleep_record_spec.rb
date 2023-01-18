1020b3c589c450719b60b17bb4eb8d8aa962237a
eason.lin <lillccian@gmail.com>
2023-01-18 20:09:40 +0800

    Add sleep_record valid spec

--- spec/models/sleep_record_spec.rb
+++ spec/models/sleep_record_spec.rb
@@ -13,5 +13,32 @@
 require 'rails_helper'
 
 RSpec.describe SleepRecord, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validate' do
+    let(:user) { create(:user) }
+    let(:record) {
+      user.sleep_records.new
+    }
+    let!(:other_record){ create(:sleep_record, user: user) }
+
+    context '#time_comparison_valid' do
+      it do
+        record.start_at = Time.current
+        record.end_at = Time.current
+
+        expect(record.save).to be_falsey
+        expect(record.errors).to include(:start_at)
+      end
+    end
+
+    context '#time_overlap_valid' do
+      it do
+        record.start_at = other_record.start_at
+        record.end_at   = other_record.end_at
+
+        expect(record.save).to be_falsey
+        expect(record.errors).to include(:start_at)
+        expect(record.errors).to include(:end_at)
+      end
+    end
+  end
 end
