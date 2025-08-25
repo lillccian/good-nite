af63f476f3416b115b9ce56cbc14af83c9e79500
eason.lin <eason.lin@phrozen3d.com>
2025-08-25 12:13:13 -0700

    Add index to follows and sleep_records

--- spec/factories/follows.rb
+++ spec/factories/follows.rb
@@ -10,7 +10,9 @@
 #
 # Indexes
 #
+#  index_follows_on_follower_id                   (follower_id)
 #  index_follows_on_follower_id_and_following_id  (follower_id,following_id) UNIQUE
+#  index_follows_on_following_id                  (following_id)
 #
 FactoryBot.define do
   factory :follow do
